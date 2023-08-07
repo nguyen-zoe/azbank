bf934a2cb2f741247e789786831d4f90eceb834e
Zoe <znguyen2007@gmail.com>
2023-08-07 16:05:22 +0100

    commit backend

--- backend/AZBank-Transaction/src/main/java/com/azbank/controller/TransactionController.java
+++ backend/AZBank-Transaction/src/main/java/com/azbank/controller/TransactionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("api/v1/transactions")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class TransactionController {
 
 	private final TransactionService service;
@@ -48,7 +50,7 @@ public class TransactionController {
 	}
 	
 	
-	@GetMapping("/accounts/{accoundId")
+	@GetMapping("/accounts/{accoundId}")
 	public ResponseEntity<List<Transaction>> findAllByAccountId(@PathVariable("accountId") Integer id){
 		return ResponseEntity.ok(service.findAllByAccount(id));
 	}
